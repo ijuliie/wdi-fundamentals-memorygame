4792c21e3967807c32d5214c8d3debccdf48337e
julie <julie@julies-MacBook-Pro.local>
2020-02-04 17:42:06 -0800

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,16 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+let = cardsInPlay = [];
 
-console.log("You picked " + cardOne);
-console.log("You picked " + cardThree);
+const cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User picked " + cardOne);
+
+const cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User picked " + cardTwo);
+
+if(cardsInPlay[0] === cardsInPlay[2]){
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again!");
+}
