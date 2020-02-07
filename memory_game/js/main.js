6707ca45f972b4a2800fe46b525fd7fa954aadbc
julie <julie@julies-MacBook-Pro.local>
2020-02-06 18:11:44 -0800

    created createBoard function and edit flipCard function

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,22 +23,43 @@ const cards = [
 let cardsInPlay = [];
 
 function checkForMatch(){
-    if(cardsInPlay[0] === cardsInPlay[2]){
+    if(cardsInPlay[0] === cardsInPlay[1]){
         alert("You found a match!");
     } else {
-        alert("Sorry, try again!");
+        alert("Sorry, try again!")
     }
 }
 
-function flipCard(cardId){
+function flipCard(){
+    let cardId = this.getAttribute("data-id");
     console.log("User flipped " + cards[cardId].rank);
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+    this.setAttribute("src", cards[cardId].cardImage);
+    if(cardsInPlay.length === 2){
+        checkForMatch();
+    }
 }
 
+function createBoard(){
+    for(let i = 0; i < cards.length; i++){
+        let cardElement = document.createElement("img");
+        cardElement.setAttribute("src", "images/back.png");
+        cardElement.setAttribute("data-id", i);
+        cardElement.addEventListener("click", flipCard);
+        document.getElementById("game-board").appendChild(cardElement);
+    }
+}
 
-flipCard(0);
-flipCard(2);
-checkForMatch();
+// function reset(){
+//     for(let i = 0; i < cardsInPlay.length; i++){
+    
+//     }
+// }
+
+// const resetButton = document.getElementById("reset");
+// resetButton.addEventListener("click", reset);
+
+createBoard();
 
